62291fcda41b2dddab7f7e3d04cd95b9b694643a
Kezhich <azerot96@gmail.com>
2020-11-26 02:54:31 +0300

    minor changes

--- extension-master/js/popup.js
+++ extension-master/js/popup.js
@@ -5,24 +5,35 @@ if(document.readyState === 'loading') {
 }
 
 function handleButton(){
+    const submarine =document.getElementById('submarine');
+    console.log(submarine)
     var isOn = parseInt(localStorage.getItem('periscope_isOn')) || 0;
     localStorage.setItem('periscope_isOn', isOn);
     const buttonEl =document.getElementById("on_off")
     console.log(isOn)
     if (parseInt(isOn))
-          {buttonEl.classList.add("btn-primary")}
+          {buttonEl.classList.add("btn-primary");
+          submarine.classList.add("floating");}
      else { 
          buttonEl.classList.remove("btn-primary")
+         submarine.classList.add("hidden")
         }
     buttonEl.addEventListener("click", function () {
         if (parseInt(isOn)) {
             isOn = 0;
             localStorage.setItem('periscope_isOn', isOn)
             buttonEl.classList.remove("btn-primary")
+            submarine.classList.remove("floating")
+            submarine.classList.add("hidden")
         } else {
             isOn = 1;
+            submarine.classList.remove("hidden")
+            submarine.classList.add("falling")
             localStorage.setItem('periscope_isOn', isOn)
             buttonEl.classList.add("btn-primary")
+            setTimeout(() => {
+                submarine.classList.add("floating")
+            }, 6000);
         }
 
     })
